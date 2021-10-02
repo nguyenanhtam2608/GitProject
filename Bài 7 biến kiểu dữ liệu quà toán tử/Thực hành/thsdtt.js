fd37247040255a057904652ef98565a9d102d288
nguyenanhtam2608 <nguyenanhtam12344321@gmail.com>
2021-10-02 15:05:59 +0700

    nat

---
+++ Bài 7 biến kiểu dữ liệu quà toán tử/Thực hành/thsdtt.js
@@ -0,0 +1,8 @@
+var width;
+var height;
+width=prompt('nhập chiều rộng');
+height=prompt('nhập chiều cao');
+var width = parseInt(width);
+var height = parseInt(height);
+var area =width*height;
+document.w rite('area=' + area);
